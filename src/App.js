0fc754905107dfb9171807cdf985c4fd5f764af8
Jessica <je.proenca@yahoo.com.br>
2022-12-02 12:29:34 -0300

    req 5

--- src/App.js
+++ src/App.js
@@ -11,16 +11,29 @@ class App extends React.Component {
     cardAttr2: '',
     cardAttr3: '',
     cardImage: '',
-    cardRare: '',
-    cardTrunfo: '',
+    cardRare: 'normal',
+    cardTrunfo: false,
+    isSaveButtonDisabled: true,
   };
+
+  validacaoBotao = () => {
+    const { cardName, cardDescription, cardImage, cardRare, cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const naovazio = cardName !== '' && cardDescription !== '' && cardImage !== '' && cardRare !== '';
+    const soma210 = (parseInt(cardAttr1) + parseInt(cardAttr2) + parseInt(cardAttr3)) <= 210;
+    const ate90 = cardAttr1 <= 90 && cardAttr2 <= 90 && cardAttr3 <= 90;
+    const maior0 = cardAttr1 >= 0 && cardAttr2 >= 0 && cardAttr3 >= 0;
+
+    this.setState({
+      isSaveButtonDisabled: !(naovazio && soma210 && ate90 && maior0),
+    })
+  }
 
   onInputChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
-    });
+    }, this.validacaoBotao);
   };
 
   render() {
